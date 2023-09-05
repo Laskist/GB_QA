09886d73230c0f84405274e8e21b698771d04f20
Laskist <111230844+Laskist@users.noreply.github.com>
2023-09-06 00:03:53 +0300

    Add files via upload

--- task 1/BaseApp.py
+++ task 1/BaseApp.py
@@ -1,5 +1,3 @@
-import logging
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -9,77 +7,29 @@ class BasePage:
         self.base_url = "https://test-stand.gb.ru"
 
     def find_element(self, locator, time=10):
-        try:
-            element = WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
-                                                             message=f"Cant find element by locator {locator}")
-        except:
-            logging.exception("Find element exception")
-            element = None
-        return element
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                        message=f"Cant find element by locator {locator}")
+
 
     def get_element_property(self, locator, property):
         element = self.find_element(locator)
-        if element:
-            return element.value_of_css_property(property)
-        else:
-            logging.error(f"Property {property} not found in element with locator {locator}")
-            return None
+        return element.value_of_css_property(property)
+
 
     def go_to_site(self):
-        try:
-            start_browsing = self.driver.get(self.base_url)
-        except:
-            logging.exception("Exception while open site")
-            start_browsing = None
-        return start_browsing
+        return self.driver.get(self.base_url)
 
-    def enter_text_into_field(self, locator, word, description=None):
-        if description:
-            element_name = description
-        else:
-            element_name = locator
-        logging.debug(f"Send '{word}' to element {element_name}")
+    def enter_text_into_field(self, locator, word):
         field = self.find_element(locator)
-        if not field:
-            logging.error(f"Element {locator} not found")
-            return False
-        try:
-            field.clear()
-            field.send_keys(word)
-        except:
-            logging.exception(f"Exception while operate with {locator}")
-            return False
-        return True
+        field.clear()
+        field.send_keys(word)
 
-    def click_button(self, locator, description=None):
-        if description:
-            element_name = description
-        else:
-            element_name = locator
+
+    def click_button(self, locator):
         button = self.find_element(locator)
-        if not button:
-            return False
-        try:
-            button.click()
-        except:
-            logging.exception(f"Exception with click")
-            return False
-        logging.debug(f"Click {element_name} button")
-        return True
+        button.click()
 
-    def get_text_from_element(self, locator, description=None):
-        if description:
-            element_name = description
-        else:
-            element_name = locator
-        field = self.find_element(locator, time=2)
-        if not field:
-            return None
-        try:
-            text = field.text
-        except:
-            logging.exception(f"Exception while get text from {element_name}")
-            return None
-        logging.debug(f"We find text {text} in field {element_name}")
-        return text
+    def get_text_from_element(self, locator):
+        return WebDriverWait(self.driver).until(EC.presence_of_element_located(locator),
+                                    message=f"Cant find element by locator {locator}")
 
